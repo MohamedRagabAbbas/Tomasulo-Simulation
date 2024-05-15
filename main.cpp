48a1a68b7f2d74db28e4b1cfec8d1f22a7824256
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-05-15 11:44:09 +0300

    added function to read from txt file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include "Instruction.h"
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+vector<string> reading_from_file(string file_name)
+{
+    ifstream file(file_name);
+    vector<string> instructions;
+    string line;
+    while (getline(file, line))
+    {
+        instructions.push_back(line);
+    }
+    return instructions;
+}
+
 int main() {
     cout << "Hello World" << std::endl;
-
     // For testing
     Instruction instr1(1, "LOAD R1,10(R2)");
     Instruction instr2(2, "BEQ R1,R2,15");
