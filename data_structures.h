8188c527eabc87c68e936fc71c062ec3d3a00e91
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-05-15 11:07:30 +0300

    removed unnecessary code and fixed issue with parser header

--- data_structures.h
+++ data_structures.h
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <stringstream>
+
 using namespace std;
 struct ReservationStation {
     int instructionId;
@@ -24,55 +24,3 @@ struct RegisterStatus {
     int q;
 };
 
-class Instruction {
-    int instructionId;
-    string name;
-    string op;
-    int dest;
-    int src1;
-    int src2;
-    int imm;
-    Instruction(int instructionId, string inst) {
-        stringstream stream(inst,',');
-        string inst_op, inst_dest, inst_src1, inst_src2;
-        ss >> inst_op;
-
-        // -->Load/store instructions
-        if(inst_op == "LOAD") {
-            ss >> dest >> src1;
-            op = given_op;
-        }
-        else if(inst_op == "STORE") {
-            ss >> src1 >> src2;
-            op = given_op;
-        }
-        // --> Conditional branch instructions
-        else if (inst_op == "BEQ") {
-            ss >> src1 >> src2 >> imm;
-            op = given_op;
-        }
-        // --> Call/Return instructions
-        else if (inst_op == "CALL") {
-            ss >> imm;
-            op = given_op;
-        }
-        else if (inst_op == "RET") {
-            op = given_op;
-        }
-        // --> Arithmetic instructions
-        else if (inst_op == "ADD" || inst_op == "MUL"|| inst_op == "NAND") {
-            ss >> dest >> src1 >> src2;
-            op = given_op;
-        }
-        else if (inst_op == "ADDI") {
-            ss >> dest >> src1 >> imm;
-            op = given_op;
-        }
-        else {
-            cout << "Invalid instruction" << endl;
-        }
-    }
-        
-    
-    
-};
