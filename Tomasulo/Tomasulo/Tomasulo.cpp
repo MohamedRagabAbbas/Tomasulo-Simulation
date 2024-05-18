907bea43b12a149fda5c3ec11a1e89112807a618
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-05-18 12:43:55 +0300

    loading is working fine

--- Tomasulo/Tomasulo/Tomasulo.cpp
+++ Tomasulo/Tomasulo/Tomasulo.cpp
@@ -803,7 +803,7 @@ void read_memory_file(string mem_file) {
 
 void taskManager() {
 
-    read_instructions_file("instructions1.txt");
+    read_instructions_file("instructions2.txt");
     read_memory_file("memory.txt");
     fillingInstructions();
     fillingReservationStation();
